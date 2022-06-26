9e1c952fb8ffc448a88cc26963f3ed5c656ec2d2
Helio Sousa <2000027@estudante.uab.pt>
2022-06-26 23:13:54 +0100

    update

---
+++ CreatingAScene/test.js
@@ -0,0 +1,22 @@
+// Our Javascript will go here.
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0xffffff, 1 ); // cor do fundo branco -> Changing three.js background to transparent or other color
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+animate();
